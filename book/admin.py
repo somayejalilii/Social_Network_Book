8b164b072a307c25d45df7d3f4d89079a6387cc9
somayejalilii <82180831+somayejalilii@users.noreply.github.com>
2021-06-25 20:37:43 +0430

    Add files via upload
    change 1 code

--- book/admin.py
+++ book/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from book.models import Book
+from book.models import Book, Author
 
+admin.site.register(Book)
+admin.site.register(Author)
 
-@admin.register(Book)
-class Book_admin(admin.ModelAdmin):
-    list_display = ['id', 'book_name']
+# @admin.register(Book)
+# class Book_admin(admin.ModelAdmin):
+#     list_display = ['id', 'name']
--- book/models.py
+++ book/models.py
@@ -1,18 +1,37 @@
 from django.db import models
-
+#from django.utils import  timezone
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField('name', max_length=30)
+
+
 class Book(models.Model):
-    book_name = models.CharField('book name', max_length=100, null=True, blank=True)
-    author_book = models.CharField('Author book', max_length=100, null=True, blank=True)
-    public_year = models.CharField('Year of publication', max_length=50)
-    time_record_book = models.DateTimeField('Time to record books')
-    user_name = models.CharField('user name', max_length=100)
-    update_time = models.DateTimeField('update time')
+    class Meta:
+        verbose_name = 'کتاب'
+        verbose_name_plural = 'کتاب ها'
+    STATUS = [('F', 'free'), ('B', 'borrwed'), ('D', 'deprecated')]
+    name = models.CharField('name', max_length=100, null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,null=True)
+    publich_year = models.DateTimeField('Year of publichation')
+    record_date = models.DateTimeField('Time to record books', auto_now_add=True)
+    update_time = models.DateTimeField('update time', auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS)
 
     @property
     def full_name(self):
-        return f'{self.book_name},{self.author_book}'
+        return f'{self.name}'
 
     def __str__(self):
-        return f'{self.book_name} and {self.update_time}'
+        return f'{self.name}'
+
+    def change_status(self):
+        if self.status == 'F':
+            self.status = 'B'
+        else:
+            self.status = 'F'
+        self.save()
+        return self.status
+
+    def get_publish_yeat(self):
+        return self.publich_year.year
